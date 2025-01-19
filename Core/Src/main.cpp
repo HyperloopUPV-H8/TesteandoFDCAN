6ca1d563b2cf096c7dbf89fadd5dfe7864d07a0d
Catonplas <dcancat@etsinf.upv.es>
2025-01-19 21:49:24 +0000

    a

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -28,19 +28,20 @@ int main(void) {
     SharedMemory::start("sim_tests","state_machine__tests");
     int id = FDCAN::inscribe(FDCAN::fdcan1); 
     STLIB::start();
-    std::string data = "Hello2";
+    //std::string data = "Hello2";
     //std::thread read_thread(r_thread,id);
     while (1) {
-        bool test = FDCAN::transmit(id, 1, data.c_str(), FDCAN::DLC::BYTES_16);
-        std::cout <<endl<< test << endl;
+        //bool test = FDCAN::transmit(id, 1, data.c_str(), FDCAN::DLC::BYTES_6);
+        //std::cout <<endl<< test << endl;
         
-        /*FDCAN::Packet packet;
-        test = FDCAN::read(id, &packet);
+        FDCAN::Packet packet;
+        bool test = FDCAN::read(id, &packet);
         std::cout <<endl<< test << endl;
         std::cout << packet.rx_data.data() << endl;
         std::cout << packet.identifier << endl;
         std::cout << packet.data_length << endl;
-        */
+        std::cout<<endl<<test<<endl;
+        
         std::this_thread::sleep_for(std::chrono::seconds(1));
         STLIB::update();
     }
