b947719d66bb120f6ff9d3a8c8623541e3c5a794
Catonplas <dcancat@etsinf.upv.es>
2025-01-21 20:11:57 +0000

    Codigo tests

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -8,49 +8,56 @@
 #include "ST-LIB.hpp"
 
 
-
-/*void r_thread(int id) {
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    while (1) {
-        FDCAN::Packet packet;
-        std::cout << "Reading data" << endl;
-        bool test = FDCAN::read(id, &packet);
-        std::cout << "data read" << endl;
-        if (test) {
-            std::cout << "Received data: " << packet.rx_data.data() << endl;
-            std::cout << "Identifier: " << packet.identifier << endl;
-            std::cout << "Data length: " << packet.data_length << endl;
-        }
-        std::this_thread::sleep_for(std::chrono::seconds(1)); 
-    }
-}
-*/
-int main(void) {
+int main1(){
+#ifdef SIM_ON
     SharedMemory::start("gpio__blinking_led","state_machine__blinking_led");
+#endif
     int id = FDCAN::inscribe(FDCAN::fdcan1); 
+    std::string data = "Hello2";
     STLIB::start();
-    //std::string data = "Hello2";
-    //std::thread read_thread(r_thread,id);
-    while (1) {
-        //bool test = FDCAN::transmit(id, 1, data.c_str(), FDCAN::DLC::BYTES_6);
-        //std::cout <<endl<< test << endl;
-        
-        FDCAN::Packet packet;
-        //bool test = FDCAN::read(id, &packet);
+    Time::register_low_precision_alarm(500,[&](){
+        FDCAN::transmit(id, 1, data.c_str(), FDCAN::DLC::BYTES_6);
+    });
+    FDCAN::Packet packet;
+    Time::register_low_precision_alarm(500,[&](){
         FDCAN::read(id, &packet);
-        for(uint8_t i = 0; i < packet.data_length; i++){
-            std::cout << /*uint16_t*/(packet.rx_data[i])<< " ";
-        }
-        
-        std::cout <<endl<<  packet.identifier << endl;
-        std::cout << packet.data_length << endl;
-        //std::cout<<endl<<test<<endl;
-        
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+    });
+    while(1){
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
         STLIB::update();
     }
-    //read_thread.join();
-}
+    }
+
+
+
+int main2(){
+#ifdef SIM_ON
+    SharedMemory::start("gpio__blinking_led","state_machine__blinking_led");
+#endif
+    enum state_id{
+        connecting =0,
+        operational =1,
+    };
+    PinState led_state = PinState::OFF;
+    uint8_t id = DigitalInput::inscribe(PA1);
+    STLIB::start();
+    StateMachine state_machine(connecting);
+    state_machine.add_state(operational);
+    state_machine.add_transition(connecting, operational, [&](){
+        return led_state;
+
+    });
+    state_machine.add_low_precision_cyclic_action([&](){
+        led_state = DigitalInput::read_pin_state(id);
+    }, std::chrono::milliseconds(1000), {connecting, operational});
+    while(1){
+        STLIB::update();
+        state_machine.check_transitions();
+        
+    }
+    }
+
 
 void Error_Handler(void) {
     ErrorHandler("HAL error handler triggered");
